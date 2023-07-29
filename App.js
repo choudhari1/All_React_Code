b362b60f5c0a313a973b68db7cb21897b92abef1
choudhari1 <57314380+choudhari1@users.noreply.github.com>
2023-07-29 18:58:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { User } from './component/User';
+import { useEffect } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+     <User/>
+    </div>
+  );
+}
+
+export default App;
